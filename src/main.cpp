210f0008b7c0ff7ee15dc334be215035ec1d1e3c
Saurabh Bagalkar <sbagalka@asu.edu>
2018-01-05 00:33:06 -0600

    start with 0 velocity

--- src/main.cpp
+++ src/main.cpp
@@ -202,7 +202,7 @@ int main() {
   }
 
   int lane = 1;
-  double ref_vel= 49.5;
+  double ref_vel= 0;
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane,&ref_vel](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
