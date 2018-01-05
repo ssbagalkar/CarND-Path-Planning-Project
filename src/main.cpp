4bcb9990c028cc2eb3487a99d8438d317c015446
Saurabh Bagalkar <sbagalka@asu.edu>
2018-01-04 22:45:02 -0600

    fill up rest of the path planner

--- src/main.cpp
+++ src/main.cpp
@@ -203,7 +203,7 @@ int main() {
 
   int lane = 1;
   double ref_vel= 49.5;
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane,&ref_vel](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -314,23 +314,61 @@ int main() {
           s.set_points(ptsx,ptsy);
 
           //Define the actual(x,y) points we will use for planner
+          vector<double> next_x_vals;
+          vector<double> next_y_vals;
+
+          // Start with all of the previous path points from last time
+          for(int i=0;i<previous_path_x.size();i++)
+          {
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
+
+          // Calculate how to break up spline points so that we travel at our desired reference velocity
+          double target_x = 30.0;
+          double target_y = s(target_x);
+          double target_dist = sqrt(pow(target_x,2)+pow(target_y,2));
+
+          double x_add_on = 0;
+
+          // Fill up rest of our path planner after filling it with previous points,here we will always output 50 points
+          for (int i=1;i<=50-previous_path_x.size();i++)
+          {
+            double N= (target_dist/(0.02*ref_vel/2.24));
+            double x_point = x_add_on+(target_x)/N;
+            double y_point=s(x_point);
+
+            x_add_on = x_point;
+
+            double x_ref = x_point;
+            double y_ref = y_point;
+
+            // rotate back to normal after rotating it earlier
+            x_point = (x_ref*cos(ref_yaw)-y_ref*sin(ref_yaw));
+            y_point = (x_ref*sin(ref_yaw)+y_ref*cos(ref_yaw));
+
+            x_point += ref_x;
+            y_point += ref_y;
+
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(x_point);
+          }
 
           	json msgJson;
 
-          	vector<double> next_x_vals;
-          	vector<double> next_y_vals;
+
 
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
-            double dist_inc = 0.5;
-            for(int i = 0; i < 50; i++)
-            {
-              double next_s = car_s + (i+1)*dist_inc;
-              double next_d = 2+4*lane;
-              vector<double> xy = getXY(next_s,next_d,map_waypoints_s,map_waypoints_x,map_waypoints_y);
-              next_x_vals.push_back(xy[0]);
-              next_y_vals.push_back(xy[1]);
-            }
+//            double dist_inc = 0.5;
+//            for(int i = 0; i < 50; i++)
+//            {
+//              double next_s = car_s + (i+1)*dist_inc;
+//              double next_d = 2+4*lane;
+//              vector<double> xy = getXY(next_s,next_d,map_waypoints_s,map_waypoints_x,map_waypoints_y);
+//              next_x_vals.push_back(xy[0]);
+//              next_y_vals.push_back(xy[1]);
+//            }
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
